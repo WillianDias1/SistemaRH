2861648afeb8662c7464212dfec6683d96008336
Luan Evangelista Dos Santos <luanevangelistads@gmail.com>
2022-11-09 13:59:28 -0300

    ultima atualização

--- src/Frontend/pagina principal/scrpt.js
+++
@@ -1,8 +0,0 @@
-const list = document.querySelectorAll('.lista');
-function activeLink(){
-    list.forEach((item) =>
-    item.classList.remove('active'));
-    this.classList.add('active');
-}
-list.forEach((item) =>
-item.addEventListener('click', activeLink));
