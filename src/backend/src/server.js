ab3a3a17605e1d52c65f4139ebddb3b1991b2778
Willian Dias <diaswillian75@gmail.com>
2022-11-16 16:44:50 -0300

    atualiazação cors server

--- src/backend/src/server.js
+++ src/backend/src/server.js
@@ -1,30 +1,9 @@
-/**
- * Driver: select * from users;
- * Query Builders: table('users').select('*').where('')
- */
+const express = require('express')
+ const routes = require('./routes')
+ const cors = require('cors')
+ const app = express();
 
-/*
-*GET: Buscar/listar uma informação no backend
-*POST: Criar uma informação no backend
-*Para comentar um trecho de código: 'Crtl+;'
-*PUT: Alterar uma informação no backend
-*DELETE: Deletar uma informação no backend
-*/
-
-/**
- * Parâmentros
- * Query: Recebe parâmentros nomeados na rota
- * Route params: parâmentros utilizados para identificar um recurso
- * Request.Body: corpo da requisição
- * 
- */
-
-const express = require('express');
-const routes = require('./routes');
-
-const app = express();
-app.use(express.json());
-
-app.use(routes);
-
-app.listen(3001);
+ app.use(express.json())
+ app.use(cors())
+ app.use(routes);
+ app.listen(3001);
